7abf7e324c71ad17e99648f64c7d4f85c59f9357
TheoLeanse <theoleanse@gmail.com>
2016-02-29 21:20:33 +0000

    basic functions

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-'use strict';
+window.onload = () => {
 
-var dog = "http://rack.3.mshcdn.com/media/ZgkyMDE1LzAyLzAzLzgzL2RvZ3RyaWNrLmUxNmFhLmdpZgpwCXRodW1iCTEyMDB4OTYwMD4/696ebca8/d40/dog-trick.gif";
-var cat = "http://i.imgur.com/4v9ftED.gif";
+    const imageNodes = document.querySelectorAll('.image');
+    const images = Array.from(imageNodes);
 
-window.onload = function () {
-    document.querySelector('.image').addEventListener('click', function (e) {
-        e.target.src === dog
-            ? e.target.src = cat
-            : e.target.src = dog;
+    images.forEach(image => {
+        image.addEventListener('click', event => {
+            if(!event.target.classList.contains('hidden')) {
+                images.forEach(element => {
+                    element.classList.toggle('hidden');
+                });
+            }
+        });
     });
 };
